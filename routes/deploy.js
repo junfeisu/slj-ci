cd3009aed6a107e93e629168e8e16b99fe2ae8c8
junfeisu <1982764150@qq.com>
2018-09-17 16:43:15 +0800

    test webhook

--- routes/deploy.js
+++ routes/deploy.js
@@ -5,6 +5,13 @@ const webhookDeploy = {
   path: '/deploy',
   method: 'POST',
   handler: (req, h) => {
+    exec('git pull', (err, stdout, stderr) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log(req.body.commits)
+      }
+    })
     // exec('git pull origin dev', {'cwd': '/home/sjffly/p'})
     const ciYaml = parse(path.resolve(process.env.PWD, './ci.yml'))
     console.log(ciYaml)
